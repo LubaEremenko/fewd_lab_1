359a20626309af143d99b1b38190f958aa86fefa
LubaEremenko <21314@student.dorset-college.ie>
2020-12-10 19:46:10 +0000

    ‘commit’

---
+++ lab1.js
@@ -0,0 +1,30 @@
+// question 3
+var firstName = 'Liubov';
+
+// question 4
+var favouriteColor = 'green';
+
+// question 5
+var favouriteFood = 'mango';
+var city = 'Paris';
+
+//question 8
+console.log('My name is ' + firstName + ' my favourite color is ' + favouriteColor); 
+console.log('I love ' + favouriteFood);
+console.log('I had never been in ' + city);
+
+//question 9
+//output to the console of three statements
+
+//question 10
+var phrase = 'A rainbow is formed by the sunlight. Shining through rain drops so small. The rain bends the light from the sunshine, and colors the sky for us all.';
+var endingPhrase = 'is what we see. Rainbow colours, are made with light bending you see!';
+var one = 'Red';
+var two = 'orange';
+var three = 'yellow';
+var four = 'green';
+var five = 'blue';
+var six = 'indigo';
+var seven = 'violet';
+
+console.log(phrase +' ' + one + ' ' + two + ' ' + three + ' ' + four + ' ' + five + ' ' + six + ' ' + seven + ' ' + endingPhrase);
